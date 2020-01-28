8a21ff4ccd91765907a07c8153ae1286cdb3ff4a
Will Alley <willalley345@gmail.com>
2020-01-28 12:48:16 -0500

    created delete task feature

--- App.js
+++ App.js
@@ -15,8 +15,8 @@ export default class App extends Component {
   }
   render () {
     return (
-      <View style={{flex: 1, backgroundColor: '#333', marginTop: 10}}>
-        <Text style={{alignSelf: 'center', padding: 5, fontSize: 25, color: '#666'}}>/*   TODO   */</Text>
+      <View style={[styles.bg, {flex: 1, paddingTop: 15}]}>
+        <Text style={[styles.bg, styles.title]}>/*   TODO   */</Text>
         <Checklist type='daily' />
       </View>
     );
@@ -29,6 +29,7 @@ class Checklist extends Component {
     this.state = {
       text: null,
       numChecks: 1,
+      deleteToggled: false,
       tasklist: null
     }
     this.addCheck = this.addCheck.bind(this);
@@ -37,6 +38,7 @@ class Checklist extends Component {
     this.updateList = this.updateList.bind(this);
     this.increaseChecks = this.increaseChecks.bind(this);
     this.decreaseChecks = this.decreaseChecks.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
   }
   componentDidMount () {
     this.updateList();
@@ -102,29 +104,28 @@ class Checklist extends Component {
       this.setState(state => ({numChecks: state.numChecks - 1}));
     }
   }
+  deleteTask (index) {
+    const task = this.state.tasklist[index];
+    db.transaction(txn => {
+      txn.executeSql(
+        "delete from tasks where id = ?;",
+        [task.id]
+      );
+    }, null, this.updateList);
+  }
   render () {
     return (
-      <View style={{flex: 1, backgroundColor: '#fff'}}>
-        <View style={[styles.border, {margin: 5}]}>
-          <ExpandingView startOpen={false} >
-            <View style={[{padding: 5, alignSelf: 'center'}]}><Text>Add New Task</Text></View>
-            <View style={{flexDirection: 'row'}}>
-              <View style={[styles.leftAlignLabel, {flex: 1}]}><Text>Frequency</Text></View>
-              <View style={{flex: 3}}>
-                <IntegerInput value={this.state.numChecks} increase={this.increaseChecks} decrease={this.decreaseChecks} />
-              </View>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-              <View style={[styles.leftAlignLabel, {flex: 1}]}><Text>Name</Text></View>
-              <View style={{flex: 3}}>
-                <TextInput style={styles.TextInput} onChangeText={text => {this.setState({text: text})}} onSubmitEditing={this.createTask} value={this.state.text}/>
-              </View>
-            </View>
-          </ExpandingView>
-        </View>
+      <View style={{flex: 1}}>
+        <AddComponentDropdown
+          frequencyInput={<IntegerInput value={this.state.numChecks} increase={this.increaseChecks} decrease={this.decreaseChecks} />}
+          nameInput={<TextInput style={[styles.TextInput, styles.yellow]} onChangeText={text => {this.setState({text: text})}} onSubmitEditing={this.createTask} value={this.state.text}/>}
+        />
+        <TouchableHighlight style={[styles.border, styles.box]} underlayColor='#555' onPress={() => {this.setState((oldState) => ({deleteToggled: !oldState.deleteToggled}))}}>
+          <Text style={styles.red}>Delete Task</Text>
+        </TouchableHighlight>
         <FlatList 
           data={this.state.tasklist} 
-          renderItem={({item, index}) => <ListItem task={item} addCheck={() => this.addCheck(index)} removeCheck={() => this.removeCheck(index)} />} 
+          renderItem={({item, index}) => <ListItem task={item} deleteToggled={this.state.deleteToggled} addCheck={() => this.addCheck(index)} removeCheck={() => this.removeCheck(index)} deleteTask={() => this.deleteTask(index)} />} 
           keyExtractor={item => `${item.id}`} 
         />
       </View>
@@ -135,14 +136,14 @@ class Checklist extends Component {
 function IntegerInput (props) {  
   return (
     <View style={[styles.IntegerInput, styles.border]}>
-      <TouchableHighlight style={[styles.IntegerInputChild, {flex: 3}]} onPress={props.decrease} underlayColor='#ddd'>
-        <Text>Down</Text>
+      <TouchableHighlight style={[styles.IntegerInputChild, {flex: 3}]} onPress={props.decrease} underlayColor='#555'>
+        <Text style={styles.buttonSymbols}>-</Text>
       </TouchableHighlight>
       <View style={[styles.IntegerInputChild, styles.leftRightBorder, {flex: 2}]}>
-        <Text>{props.value}</Text>
+        <Text style={styles.grey}>{props.value}</Text>
       </View>
-      <TouchableHighlight style={[styles.IntegerInputChild, {flex: 3}]} onPress={props.increase} underlayColor='#ddd'>
-        <Text>Up</Text>
+      <TouchableHighlight style={[styles.IntegerInputChild, {flex: 3}]} onPress={props.increase} underlayColor='#555'>
+        <Text style={[styles.buttonSymbols]}>+</Text>
       </TouchableHighlight>
     </View>
   );
@@ -152,17 +153,49 @@ function ListItem (props) {
   const checkboxes = Array(props.task.numChecks).fill(null).map((val, i) => 
     <View key={props.task.id + '_' + i} style={[styles.checkBox, i < props.task.checksDone ? styles.checkN : styles.checkY]}></View>
   );
+  const checkBoxSection = (
+    <View style={[styles.checks, {flex: 1}]}>
+      {checkboxes}
+    </View>
+  );
+  const deleteTaskSection = (
+    <TouchableHighlight style={[styles.checks, {flex: 1, justifyContent: 'center'}]} underlayColor='#555' onPress={props.deleteTask}>
+      <Text style={[styles.red]}>delete</Text>
+    </TouchableHighlight>
+  )
   return (
-    <TouchableHighlight onPress={props.addCheck} onLongPress={props.removeCheck} underlayColor='#ddd'>
+    <TouchableHighlight disabled={props.deleteToggled} onPress={props.addCheck} onLongPress={props.removeCheck} underlayColor='#555' style={{margin: 5, borderRadius: 2}}>
       <View style={[styles.listItem, styles.border]}>
-        <View style={[styles.checks, {flex: 1}]}>
-          {checkboxes}
-        </View>
+        {props.deleteToggled ? deleteTaskSection : checkBoxSection}
         <View style={[styles.leftAlignLabel, {flex: 4}]}>
-          <Text>{props.task.name}</Text>
+          <Text style={styles.yellow}>{props.task.name}</Text>
         </View>
       </View>
     </TouchableHighlight>
+  );
+}
+
+function AddComponentDropdown (props) {
+  return (
+    <View style={[styles.border, {margin: 5}]}>
+      <ExpandingView startOpen={false} >
+        <View style={[{padding: 5, alignSelf: 'center'}]}>
+          <Text style={styles.blue}>Add New Task</Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <View style={[styles.leftAlignLabel, {flex: 1}]}><Text style={styles.grey}>Frequency</Text></View>
+          <View style={{flex: 3}}>
+            {props.frequencyInput}
+          </View>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <View style={[styles.leftAlignLabel, {flex: 1}]}><Text style={styles.grey}>Name</Text></View>
+          <View style={{flex: 3}}>
+            {props.nameInput}
+          </View>
+        </View>
+      </ExpandingView>
+    </View>
   );
 }
 
@@ -179,7 +212,7 @@ class ExpandingView extends Component {
   }
   render () {
     return (
-      <TouchableHighlight onPress={this.toggleExpand} underlayColor='#fff'>
+      <TouchableHighlight onPress={this.toggleExpand} underlayColor='#555'>
         <View>
           {React.Children.map(this.props.children, (child, index) => index === 0 || this.state.expanded ? child : null)}
         </View>
@@ -194,6 +227,27 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     borderColor: 'gray',
   },
+  bg: {
+    backgroundColor: '#333'
+  },
+  yellow: {
+    color: '#ba7'
+  },
+  blue: {
+    color: '#6ad'
+  },
+  grey: {
+    color: '#bbb'
+  },
+  red: {
+    color: '#b57'
+  },
+  buttonSymbols: {
+    fontWeight: '500', 
+    fontSize: 20,
+    color: '#6ad',
+    marginTop: -2
+  },
   leftRightBorder: {
     borderRightWidth: 0.5,
     borderLeftWidth: 0.5,
@@ -203,9 +257,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 5,
   },
+  title: {
+    alignSelf: 'center', 
+    padding: 5, 
+    fontSize: 25, 
+    color: '#999'
+  },
+  box: {
+    margin: 5, 
+    padding: 5, 
+    alignItems: 'center'
+  },
   TextInput: {
     height: 30,
     margin: 5,
+    padding: 5,
     borderRadius: 2,
     borderWidth: 0.5,
     borderColor: 'gray',
@@ -213,7 +279,7 @@ const styles = StyleSheet.create({
   listItem: {
     flexDirection: 'row',
     height: 40,
-    margin: 5,
+    // margin: 5,
   },
   checks: {
     flexDirection: 'row',
@@ -238,13 +304,14 @@ const styles = StyleSheet.create({
     height: 10, 
     borderRadius: 2, 
     borderWidth: 1, 
-    borderColor: '#555', 
+    borderColor: '#aaa', 
     margin: 1,
   },
   checkN: {
-    backgroundColor: '#6b8',
+    backgroundColor: '#aaa',
+    // backgroundColor: '#4a0',
   },
   checkY: {
-    backgroundColor: '#fff',
+    // backgroundColor: '#333',
   }
 });
